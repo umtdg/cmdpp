5e328b0d355ac7bf797c74f2ca87a23bcbae671a
Umut Dag <dagumut604@gmail.com>
2021-05-03 12:00:51 +0300

    Remove template overload of split function

--- src/utils.cpp
+++ src/utils.cpp
@@ -6,7 +6,11 @@ namespace libcmd {
 
     std::vector<std::string> split(const std::string &s, char delim) {
         std::vector<std::string> tokens;
-        split(s, delim, std::back_inserter(tokens));
+        std::istringstream iss(s);
+        std::string token;
+        while (std::getline(iss, token, delim)) {
+            tokens.push_back(token);
+        }
         return tokens;
     }
 
